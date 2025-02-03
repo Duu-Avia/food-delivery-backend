10a9dfd1e3ad161d26d0c34521cf9b3e054a9ba0
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2025-02-03 18:43:27 +0800

    test10

--- dist/models/Food-category.js
+++ dist/models/Food-category.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FOOD_CATEGORY_MODEL = exports.FOOD_CATEGORY_SCHEMA = void 0;
 const mongoose_1 = require("mongoose");
-exports.FOOD_CATEGORY_SCHEMA = new mongoose_1.Schema({
+exports.FOOD_CATEGORY_SCHEMA = new mongoose_1.Schema(
+  {
     categoryName: { type: String },
-}, { versionKey: false });
-exports.FOOD_CATEGORY_MODEL = (0, mongoose_1.model)("food-category", exports.FOOD_CATEGORY_SCHEMA, "food-category");
+  },
+  { versionKey: false }
+);
+exports.FOOD_CATEGORY_MODEL = (0, mongoose_1.model)("Food-category", exports.FOOD_CATEGORY_SCHEMA, "food-category");
