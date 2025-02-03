063203fba7aa43b6441206cc359bb62584433205
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2025-02-03 18:47:29 +0800

    test20

--- dist/index.js
+++ dist/index.js
@@ -1,40 +1,16 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("dotenv");
@@ -49,17 +25,17 @@ const cors = require("cors");
 (0, dotenv_1.configDotenv)();
 app.use(express_1.default.json());
 app.use(cors());
-const connectMongoDB = () =>
-  __awaiter(void 0, void 0, void 0, function* () {
+const connectMongoDB = () => __awaiter(void 0, void 0, void 0, function* () {
     const MONGODB_URL = process.env.MONGODB_URI;
     if (!MONGODB_URL) {
-      throw new Error("Database connection URI is failed");
+        throw new Error("Database connection URI is failed");
     }
     try {
-      yield mongoose_1.default.connect(MONGODB_URL);
-      console.log("Database connected successfully");
-    } catch (error) {}
-  });
+        yield mongoose_1.default.connect(MONGODB_URL);
+        console.log("Database connected successfully");
+    }
+    catch (error) { }
+});
 connectMongoDB();
 //food category path----------------
 app.use("/food_category", Food_category_1.foodCategoryRouter);
@@ -68,5 +44,5 @@ app.use("/dishes", Food_1.foodRouter);
 //order path----------------
 app.use("/order", Food_order_1.foodOrder);
 app.listen(port, () => {
-  console.log(`listening port ${port}`);
+    console.log(`listening port ${port}`);
 });
