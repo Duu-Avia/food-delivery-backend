4f2c94d8cddcb6731aa58df07f3294a7cf45f798
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2025-02-03 15:00:57 +0800

    setting render2

--- models/Food-category.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FOOD_CATEGORY_MODEL = exports.FOOD_CATEGORY_SCHEMA = void 0;
-const mongoose_1 = require("mongoose");
-exports.FOOD_CATEGORY_SCHEMA = new mongoose_1.Schema({
-    categoryName: { type: String },
-}, { versionKey: false });
-exports.FOOD_CATEGORY_MODEL = (0, mongoose_1.model)("food-category", exports.FOOD_CATEGORY_SCHEMA, "food-category");
--- router/Food-Category.ts
+++ router/Food-Category.ts
@@ -10,14 +10,14 @@ foodCategoryRouter.get("/", async (req: Request, res: Response) => {
   res.json(foodCategory);
 });
 
-foodCategoryRouter.put("/:id",auth, async (req: Request, res: Response) => {
+foodCategoryRouter.put("/:id", auth, async (req: Request, res: Response) => {
   const { id } = req.params;
   const { categoryName } = req.body;
   const updatedItem = await FOOD_CATEGORY_MODEL.findByIdAndUpdate(id, { categoryName }, { new: true });
   res.send(updatedItem);
 });
 
-foodCategoryRouter.delete("/:id",auth, async (req: Request, res: Response) => {
+foodCategoryRouter.delete("/:id", auth, async (req: Request, res: Response) => {
   const { id } = req.params;
   await FOOD_CATEGORY_MODEL.findByIdAndDelete(id);
   res.send("deleted category");
@@ -30,4 +30,3 @@ foodCategoryRouter.post("/", auth, async (req: Request, res: Response) => {
   });
   res.json({ newItem });
 });
-
